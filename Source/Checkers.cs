53e39b372017c9e73a3075f7244091d9b94433d1
NathanKell <nathankell@gmail.com>
2014-07-17 17:19:29 -0400

    Update plugin for .24

--- Source/Checkers.cs
+++ Source/Checkers.cs
@@ -1,56 +1,55 @@
 ﻿/**
-* Copyright (c) 2014, Majiir
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without modification, are permitted
-* provided that the following conditions are met:
-*
-* 1. Redistributions of source code must retain the above copyright notice, this list of
-* conditions and the following disclaimer.
-*
-* 2. Redistributions in binary form must reproduce the above copyright notice, this list of
-* conditions and the following disclaimer in the documentation and/or other materials provided
-* with the distribution.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
-* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-* POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Copyright (c) 2014, Majiir
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted 
+ * provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this list of 
+ * conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of 
+ * conditions and the following disclaimer in the documentation and/or other materials provided 
+ * with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
 /*-----------------------------------------*\
-| SUBSTITUTE YOUR MOD'S NAMESPACE HERE. |
+|   SUBSTITUTE YOUR MOD'S NAMESPACE HERE.   |
 \*-----------------------------------------*/
-namespace RealSolarSystem
+namespace Compatibility
 {
 
     /**
-    * This utility displays a warning with a list of mods that determine themselves
-    * to be incompatible with the current running version of Kerbal Space Program.
-    *
-    * See this forum thread for details:
-    * http://forum.kerbalspaceprogram.com/threads/65395-Voluntarily-Locking-Plugins-to-a-Particular-KSP-Version
-    */
+     * This utility displays a warning with a list of mods that determine themselves
+     * to be incompatible with the current running version of Kerbal Space Program.
+     * 
+     * See this forum thread for details:
+     * http://forum.kerbalspaceprogram.com/threads/65395-Voluntarily-Locking-Plugins-to-a-Particular-KSP-Version
+     */
 
-    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
     internal class CompatibilityChecker : MonoBehaviour
     {
         public static bool IsCompatible()
         {
             /*-----------------------------------------------*\
-            | BEGIN IMPLEMENTATION-SPECIFIC EDITS HERE. |
+            |    BEGIN IMPLEMENTATION-SPECIFIC EDITS HERE.    |
             \*-----------------------------------------------*/
 
             // TODO: Implement your own compatibility check.
@@ -58,19 +57,31 @@ namespace RealSolarSystem
             // If you want to disable some behavior when incompatible, other parts of the plugin
             // should query this method:
             //
-            // if (!CompatibilityChecker.IsCompatible()) {
-            // ...disable some features...
-            // }
+            //    if (!CompatibilityChecker.IsCompatible()) {
+            //        ...disable some features...
+            //    }
             //
-            // Even if you don't lock down functionality, you should return true if your users
+            // Even if you don't lock down functionality, you should return true if your users 
             // can expect a future update to be available.
             //
+            return Versioning.version_major == 0 && Versioning.version_minor == 24 && Versioning.Revision <= 0;
 
-            if (Versioning.version_minor != 23)
-            {
-                return false;
-            }
-            return true;
+            /*-----------------------------------------------*\
+            | IMPLEMENTERS SHOULD NOT EDIT BEYOND THIS POINT! |
+            \*-----------------------------------------------*/
+        }
+
+        public static bool IsUnityCompatible()
+        {
+            /*-----------------------------------------------*\
+            |    BEGIN IMPLEMENTATION-SPECIFIC EDITS HERE.    |
+            \*-----------------------------------------------*/
+
+            // TODO: Implement your own Unity compatibility check.
+            //
+            if (Application.unityVersion.Equals("4.5.2f1"))
+                return true;
+            return false;
 
             /*-----------------------------------------------*\
             | IMPLEMENTERS SHOULD NOT EDIT BEYOND THIS POINT! |
@@ -78,7 +89,7 @@ namespace RealSolarSystem
         }
 
         // Version of the compatibility checker itself.
-        private static int _version = 2;
+        private static int _version = 3;
 
         public void Start()
         {
@@ -122,12 +133,51 @@ namespace RealSolarSystem
                 .Select(m => m.DeclaringType.Assembly.GetName().Name)
                 .ToArray();
 
+            // A mod is incompatible with Unity if its compatibility checker has an IsUnityCompatible method which returns false.
+            String[] incompatibleUnity =
+                fields
+                .Select(f => f.DeclaringType.GetMethod("IsUnityCompatible", Type.EmptyTypes))
+                .Where(m => m != null)  // Mods without IsUnityCompatible() are assumed to be compatible.
+                .Where(m => m.IsStatic)
+                .Where(m => m.ReturnType == typeof(bool))
+                .Where(m =>
+                {
+                    try
+                    {
+                        return !(bool)m.Invoke(null, new object[0]);
+                    }
+                    catch (Exception e)
+                    {
+                        // If a mod throws an exception from IsUnityCompatible, it's not compatible.
+                        Debug.LogWarning(String.Format("[CompatibilityChecker] Exception while invoking IsUnityCompatible() from '{0}':\n\n{1}", m.DeclaringType.Assembly.GetName().Name, e));
+                        return true;
+                    }
+                })
+                .Select(m => m.DeclaringType.Assembly.GetName().Name)
+                .ToArray();
+
             Array.Sort(incompatible);
+            Array.Sort(incompatibleUnity);
+
+            String message = "Some installed mods may be incompatible with this version of Kerbal Space Program. Features may be broken or disabled. Please check for updates to the listed mods.";
 
             if (incompatible.Length > 0)
             {
                 Debug.LogWarning("[CompatibilityChecker] Incompatible mods detected: " + String.Join(", ", incompatible));
-                PopupDialog.SpawnPopupDialog("Incompatible Mods Detected", "Some installed mods are incompatible with this version of Kerbal Space Program. Some features may be broken or disabled. Please check for updates to the following mods:\n\n" + String.Join("\n", incompatible), "OK", false, HighLogic.Skin);
+                message += String.Format("\n\nThese mods are incompatible with KSP {0}.{1}.{2}:\n\n", Versioning.version_major, Versioning.version_minor, Versioning.Revision);
+                message += String.Join("\n", incompatible);
+            }
+
+            if (incompatibleUnity.Length > 0)
+            {
+                Debug.LogWarning("[CompatibilityChecker] Incompatible mods (Unity) detected: " + String.Join(", ", incompatibleUnity));
+                message += String.Format("\n\nThese mods are incompatible with Unity {0}:\n\n", Application.unityVersion);
+                message += String.Join("\n", incompatibleUnity);
+            }
+
+            if ((incompatible.Length > 0) || (incompatibleUnity.Length > 0))
+            {
+                PopupDialog.SpawnPopupDialog("Incompatible Mods Detected", message, "OK", true, HighLogic.Skin);
             }
         }
 
