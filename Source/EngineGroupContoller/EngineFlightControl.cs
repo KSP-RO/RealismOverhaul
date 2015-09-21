2d50428f20863e74665efe45ff1e9498e9355666
asmi84 <Andrey.Smirnov.84@gmail.com>
2015-09-21 09:15:37 -0400

    fixed Engine Groups Controller for  v1.0.x

--- Source/EngineGroupContoller/EngineFlightControl.cs
+++ Source/EngineGroupContoller/EngineFlightControl.cs
@@ -5,7 +5,7 @@ using System.Text;
 using UnityEngine;
 using UnityGUIFramework;
 
-namespace EngineGroupContoller
+namespace EngineGroupController
 {
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     //[KSPAddon(KSPAddon.Startup.MainMenu, false)]
@@ -29,19 +29,23 @@ namespace EngineGroupContoller
             GameEvents.onVesselWasModified.Add(OnVesselWasModified);
             _gui.StartPostDisplay(1);
             GameEvents.onGUIApplicationLauncherReady.Add(OnGUIAppLauncherReady);
+            OnGUIAppLauncherReady();
         }
 
         private void OnGUIAppLauncherReady()
         {
-            _appLauncherButton = ApplicationLauncher.Instance.AddModApplication(() => _displayGUI = true,
-                () => _displayGUI = false,
-                () => { },
-                () => { },
-                () => { },
-                () => { },
-                ApplicationLauncher.AppScenes.FLIGHT,
-                GameDatabase.Instance.GetTexture("EngineGroupContoller/ToolbarIcon4", false)
-                );
+            if (ApplicationLauncher.Ready && _appLauncherButton == null)
+            {
+                _appLauncherButton = ApplicationLauncher.Instance.AddModApplication(() => _displayGUI = true,
+                    () => _displayGUI = false,
+                    () => { },
+                    () => { },
+                    () => { },
+                    () => { },
+                    ApplicationLauncher.AppScenes.FLIGHT,
+                    GameDatabase.Instance.GetTexture("EngineGroupController/ToolbarIcon4", false)
+                    );
+            }
         }
 
         private void OnVesselWasModified(Vessel data)
