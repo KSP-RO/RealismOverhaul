513435eb42e4043ae2267a8f9a5e03b43838cc5e
Mike <11480934+MikeOnTea@users.noreply.github.com>
2019-04-01 19:16:18 +0200

    Update tech levels

--- Source/DataTransmitterRO/TechLevel.cs
+++ Source/DataTransmitterRO/TechLevel.cs
@@ -35,14 +35,14 @@ namespace RealismOverhaul.DataTransmitterRO
 
         private static IList<TechLevel> _techLevels = new List<TechLevel>()
         {
-            new TechLevel(1/10f, -13f, 4, 0.1f, 21.7f/18, 30.7f, 10.3f, 8415f),
-            new TechLevel(1/8f, -10f, 4, 1, 21.7f/18, 30.7f, 10.3f, 8415f),
-            new TechLevel(1/7f, -4.5f, 1, 5, 21.7f/18, 30.7f, 10.3f, 8415f),
-            new TechLevel(1/6f, -2.5f, 8, 5, 21.7f/18, 30.7f, 10.3f, 8415f),
-            new TechLevel(1/5.5f, 0f, 8, 10, 21.7f/18, 30.7f, 10.3f, 8415f),
-            new TechLevel(1/4.5f, 3.2f, 16, 20, 21.7f/18, 30.7f, 10.3f, 8415f),
-            new TechLevel(1/4f, 6f, 128, 40, 21.7f/18, 30.7f, 10.3f, 8415f),
-            new TechLevel(1/3f, 7f, 128, 80, 21.7f/18, 30.7f, 10.3f, 8415f)
+            new TechLevel(1/18f, -13f, 4, 0.1f, 1.6f, 1, 20.5f, 8415f),
+            new TechLevel(1/13f, -10f, 4, 1, 1.34f, 0.26f, 20.5f, 8415f),
+            new TechLevel(1/10f, -4.5f, 1, 5, 1.16f, 6.9f, 20.5f, 8415f),
+            new TechLevel(3/21.5f, -2.5f, 8, 5, 1, 20.2f, 20.5f, 8415f),
+            new TechLevel(1/6f, 0f, 8, 10, 0.86f, 17.2f, 20.5f, 8415f),
+            new TechLevel(1/4.5f, 3.2f, 16, 20, 0.75f, 21, 21, 8415f),
+            new TechLevel(1/4f, 6f, 16, 40, 11.6f/18, 30.7f, 21.4f, 8415f),
+            new TechLevel(20/53.7f, 7f, 16, 80, 10.8f/20, 21.3f, 18.3f, 8415f)
         };
     }
 }
