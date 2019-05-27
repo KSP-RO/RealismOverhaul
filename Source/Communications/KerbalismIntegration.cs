f92945f5dee724ceac3fa6b23226d5a8d6088b3c
Mike <11480934+MikeOnTea@users.noreply.github.com>
2019-05-27 07:11:25 +0200

    Kerbalism integration

---
+++ Source/Communications/KerbalismIntegration.cs
@@ -0,0 +1,307 @@
+﻿using CommNet;
+using KSP.Localization;
+using RealismOverhaul.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace RealismOverhaul.Communications
+{
+    [KSPScenario(ScenarioCreationOptions.AddToAllGames | ScenarioCreationOptions.AddToAllMissionGames, new GameScenes[] { GameScenes.FLIGHT, GameScenes.TRACKSTATION, GameScenes.SPACECENTER, GameScenes.EDITOR })]
+    class KerbalismIntegration : ScenarioModule
+    {
+        private static Cache cache = new Cache();
+        private static Assembly _kerbalismAssembly;
+
+        public void Start()
+        {
+            StartKerbalismIntegration();
+        }
+
+        private class Cache {
+            private Storage<Guid, ICollection<CachedAntenna>> _antennaCache = new Storage<Guid, ICollection<CachedAntenna>>();
+            public Storage<CommNode, float> _dataRateCache = new Storage<CommNode, float>();
+
+            public StorageView<Vessel, Guid, ICollection<CachedAntenna>> AntennaCache { get; }
+
+            public Cache()
+            {
+                AntennaCache = StorageView.Create(_antennaCache, (Vessel x) => x.id);
+            }
+
+            public void Clear()
+            {
+                _antennaCache.Clear();
+                _dataRateCache.Clear();
+            }
+
+            public void Remove(Vessel vessel)
+            {
+                _antennaCache.Remove(vessel.id);
+                RemoveDataRateCacheEntry(vessel);
+            }
+
+            private void RemoveDataRateCacheEntry(Vessel vessel)
+            {
+                var commNode = GetCommNode(vessel);
+                if (commNode != null)
+                {
+                    _dataRateCache.Remove(commNode);
+                }
+            }
+
+            private static CommNode GetCommNode(Vessel vessel) => vessel.connection?.Comm;
+
+            public void Remove(ProtoVessel protoVessel) => _antennaCache.Remove(protoVessel.vesselID);
+        }
+
+        private struct CachedAntenna
+        {
+            public ProtoPartSnapshot Snapshot { get; }
+            public ModuleConfigurableAntenna Antenna { get; }
+
+            public CachedAntenna(ModuleConfigurableAntenna antenna, ProtoPartSnapshot snapshot)
+            {
+                Snapshot = snapshot;
+                Antenna = antenna;
+            }
+        }
+
+        private class AntennaInfo
+        {
+            public double Strength { get; set; }
+            public double Rate { get; set; }
+            public int Status { get; set; }
+            public bool Linked { get; set; }
+            public double PowerUsed { get; set; }
+            public string Target { get; set; }
+            public IList<string[]> ControlPath { get; set; }
+
+            public AntennaInfo(int status) => Status = status;
+        }
+
+        public static void StartKerbalismIntegration()
+        {
+            _kerbalismAssembly = AssemblyLoader.loadedAssemblies.FirstOrDefault(x => x.name.StartsWith("Kerbalism") && x.name[9] != 'B')?.assembly;
+            if (_kerbalismAssembly?.GetType("KERBALISM.API") is Type api)
+            {
+                AddCallbacks();
+                AddCommsHandler(api);
+            }
+        }
+
+        private static void AddCallbacks()
+        {
+            GameEvents.onVesselChange.Add(v => cache.Remove(v));
+            GameEvents.onVesselStandardModification.Add(v => cache.Remove(v));
+            GameEvents.onGameSceneSwitchRequested.Add(_ => cache.Clear());
+            GameEvents.onVesselRecovered.Add((v, _) => cache.Remove(v));
+            GameEvents.onVesselTerminated.Add(v => cache.Remove(v));
+            GameEvents.onVesselWillDestroy.Add(v => cache.Remove(v));
+            GameEvents.onPartCouple.Add(x => cache.Clear());
+            GameEvents.onPartDie.Add(x => RemovePartFromCache(x));
+        }
+
+        private static void RemovePartFromCache(Part x) => cache.Remove(GetVessel(x));
+
+        private static Vessel GetVessel(Part x) => x.vessel;
+
+        private static void AddCommsHandler(Type api)
+        {
+            var comm = api.GetField("Comm", BindingFlags.Static | BindingFlags.Public).GetValue(null);
+            var addMethod = comm.GetType().GetMethod("Add", BindingFlags.Instance | BindingFlags.Public);
+            var handlerMethod = typeof(KerbalismIntegration).GetMethod(nameof(HandleCommQuery), BindingFlags.Static | BindingFlags.Public);
+            addMethod.Invoke(comm, new object[] { handlerMethod });
+        }
+
+        public static void HandleCommQuery(object kerbalismAntennaInfo, Vessel vessel)
+        {
+            var vesselSpecs = GetAntennaSpecs(vessel);
+            var powered = kerbalismAntennaInfo.GetField<bool>("powered");
+            var antennaInfo = GetAntennaInfo(vessel, vesselSpecs, powered);
+            SetKerbalismFields(kerbalismAntennaInfo, antennaInfo);
+        }
+
+        private static void SetKerbalismFields(object kerbalismAntennaInfo, AntennaInfo antennaInfo)
+        {
+            kerbalismAntennaInfo.SetField("target_name", antennaInfo.Target);
+            kerbalismAntennaInfo.SetField("strength", antennaInfo.Strength);
+            kerbalismAntennaInfo.SetField("rate", antennaInfo.Rate / 1024 / 1024 / 8);
+            kerbalismAntennaInfo.SetField("status", antennaInfo.Status);
+            kerbalismAntennaInfo.SetField("linked", antennaInfo.Linked);
+            kerbalismAntennaInfo.SetField("ec", antennaInfo.PowerUsed / 1000);
+            kerbalismAntennaInfo.SetField("control_path", antennaInfo.ControlPath);
+        }
+
+        private static string GetConnectionTargetName(Vessel vessel) => Localizer.Format(GetFirstHopNode(vessel).name).Replace("Kerbin", "DSN");
+        private static CommNode GetFirstHopNode(Vessel vessel) => GetControlPath(vessel).First.end;
+        private static CommPath GetControlPath(Vessel vessel) => vessel.connection.ControlPath;
+
+        private static AntennaInfo GetAntennaInfo(Vessel v, AntennaSpecs vesselSpecs, bool powered)
+        {
+            if (!powered || v.connection == null)
+            {
+                return new AntennaInfo(2);
+            }
+
+            return GetConnectedAntennaInfo(v, vesselSpecs);
+        }
+
+        private static AntennaInfo GetConnectedAntennaInfo(Vessel v, AntennaSpecs vesselSpecs)
+        {
+            var antennaInfo = new AntennaInfo(2);
+
+            if (!v.loaded)
+            {
+                v.connection.SetField("unloadedDoOnce", true);
+            }
+            if (v.connection.IsConnected)
+            {
+                antennaInfo.Linked = true;
+                var link = v.connection.ControlPath.First;
+                antennaInfo.Status = link.hopType == HopType.Home ? 0 : 1;
+                antennaInfo.Strength = link.signalStrength;
+                antennaInfo.Rate = GetRate(v, vesselSpecs, link);
+                antennaInfo.Target = GetConnectionTargetName(v);
+            }
+            else if (v.connection.InPlasma)
+            {
+                antennaInfo.Status = 3;
+            }
+
+            antennaInfo.ControlPath = GetControlPath(v, antennaInfo);
+
+            return antennaInfo;
+        }
+
+        private static float GetRate(Vessel vessel, AntennaSpecs vesselSpecs, CommLink link)
+        {
+            var rate = GetFirstHopRate(vesselSpecs, link);
+            if (link.hopType != HopType.Home)
+            {
+                var prevHopRate = cache._dataRateCache.Get(vessel.Connection.ControlPath.First.end);
+                rate = Mathf.Min(prevHopRate, rate);
+            }
+            cache._dataRateCache.Add(vessel.connection.Comm, rate);
+
+            return rate;
+        }
+
+        private static float GetFirstHopRate(AntennaSpecs vesselSpecs, CommLink link)
+        {
+            var preciseRate = vesselSpecs.MinDataRate * Mathf.Pow(1 - (float) link.signalStrength, -2);
+            var powerOf2Rate = Mathf.Floor(preciseRate.ToLog2()).FromLog2();
+            return Mathf.Min(vesselSpecs.MaxDataRate, powerOf2Rate);
+        }
+
+        private static IList<string[]> GetControlPath(Vessel v, AntennaInfo antennaInfo)
+        {
+            var path = new List<string[]>();
+            foreach (var link in v.connection.ControlPath)
+            {
+                var controlPathItem = GetControlPathItem(link);
+                path.Add(controlPathItem);
+            }
+            return path;
+        }
+
+        private static string[] GetControlPathItem(CommLink link)
+        {
+            double antennaPower = link.end.isHome ? link.start.antennaTransmit.power + link.start.antennaRelay.power : link.start.antennaTransmit.power;
+            double signalStrength = 1 - ((link.start.position - link.end.position).magnitude / Math.Sqrt(antennaPower * link.end.antennaRelay.power));
+            signalStrength = (3 - (2 * signalStrength)) * Math.Pow(signalStrength, 2);
+
+            string name = link.end.name.Replace("Kerbin", "DSN");
+            string value = $"{(Math.Ceiling(signalStrength * 10000) / 10000):0.00}";
+            string tooltip = "Distance: " + (link.start.position - link.end.position).magnitude.Format("m") +
+                "\nMy Max Distance: " + Math.Sqrt((link.start.antennaTransmit.power + link.start.antennaRelay.power) * link.end.antennaRelay.power).Format("m");
+            var controlPathItem = new string[] { name, value, tooltip };
+            return controlPathItem;
+        }
+
+        private static AntennaSpecs GetAntennaSpecs(Vessel v)
+        {
+            var cachedAntennas = cache.AntennaCache.GetOrAdd(v, GetAntennas);
+            var vesselSpec = new AntennaSpecs(0, 0, 0, float.MaxValue, float.MaxValue);
+            foreach (var cachedAntenna in cachedAntennas)
+            {
+                DisableAntennaUI(cachedAntenna);
+                var antennaSpecs = GetAntennaSpecs(cachedAntenna);
+                var deployState = GetDeployState(cachedAntenna);
+                if (deployState == null || deployState == ModuleDeployablePart.DeployState.EXTENDED)
+                {
+                    vesselSpec.MinDataRate = Mathf.Min(vesselSpec.MinDataRate, antennaSpecs.MinDataRate);
+                    vesselSpec.MaxDataRate = Mathf.Min(vesselSpec.MaxDataRate, antennaSpecs.MaxDataRate);
+                    vesselSpec.TotalPower += antennaSpecs.TotalPower;
+                }
+            }
+
+            return vesselSpec;
+        }
+
+        private static ModuleDeployablePart.DeployState? GetDeployState(CachedAntenna antennaInfo)
+        {
+            return antennaInfo.Snapshot == null ? GetLoadedDeployState(antennaInfo) : GetUnloadedDeployState(antennaInfo);
+        }
+
+        private static ModuleDeployablePart.DeployState? GetLoadedDeployState(CachedAntenna antennaInfo) => antennaInfo.Antenna.part.FindModuleImplementing<ModuleDeployableAntenna>()?.deployState;
+
+        private static ModuleDeployablePart.DeployState? GetUnloadedDeployState(CachedAntenna antennaInfo)
+        {
+            var moduleSnapShot = antennaInfo.Snapshot.FindModule("ModuleDeployableAntenna");
+            return moduleSnapShot?.GetString("deployState").ToEnum<ModuleDeployablePart.DeployState>();
+        }
+
+        private static AntennaSpecs GetAntennaSpecs(CachedAntenna antennaInfo)
+        {
+            return antennaInfo.Snapshot == null ? antennaInfo.Antenna.GetAntennaSpecs() : antennaInfo.Antenna.GetAntennaSpecs(antennaInfo.Snapshot.FindModule(nameof(ModuleConfigurableAntenna)));
+        }
+
+        private static void DisableAntennaUI(CachedAntenna cachedAntenna)
+        {
+            if(cachedAntenna.Snapshot != null)
+            {
+                return;
+            }
+            var antenna = cachedAntenna.Antenna;
+            antenna.Events["TransmitIncompleteToggle"].active = false;
+            antenna.Events["StartTransmission"].active = false;
+            antenna.Events["StopTransmission"].active = false;
+            antenna.Actions["StartTransmissionAction"].active = false;
+        }
+
+        private static ICollection<CachedAntenna> GetAntennas(Vessel v)
+        {
+            return v.loaded ? GetLoadedAntennas(v) : GetUnloadedAntennas(v);
+        }
+
+        private static ICollection<CachedAntenna> GetUnloadedAntennas(Vessel v)
+        {
+            var antennaInfos = new List<CachedAntenna>();
+            foreach (var partSnapshot in v.protoVessel.protoPartSnapshots)
+            {
+                var partPrefab = PartLoader.getPartInfoByName(partSnapshot.partName).partPrefab;
+
+                foreach (var antenna in partPrefab.FindModulesImplementing<ModuleConfigurableAntenna>())
+                {
+                    antennaInfos.Add(new CachedAntenna(antenna, partSnapshot));
+                }
+            }
+
+            return antennaInfos;
+        }
+
+        private static ICollection<CachedAntenna> GetLoadedAntennas(Vessel v)
+        {
+            var antennaInfos = new List<CachedAntenna>();
+            foreach (var antenna in v.FindPartModulesImplementing<ModuleConfigurableAntenna>())
+            {
+                antennaInfos.Add(new CachedAntenna(antenna, null));
+            }
+
+            return antennaInfos;
+        }
+    }
+}
--- Source/Communications/TechLevel.cs
+++ Source/Communications/TechLevel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using RealismOverhaul.Utils;
+using System.Collections.Generic;
 
 namespace RealismOverhaul.Communications
 {
@@ -7,6 +8,7 @@ namespace RealismOverhaul.Communications
         public float Efficiency { get; private set; }
         public float Gain { get; private set; }
         public float MinDataRate { get; private set; }
+        public float MaxDataRate { get; private set; }
         public float MaxPower { get; private set; }
         public float MassPerWatt { get; private set; }
         public float BaseMass { get; private set; }
@@ -15,11 +17,12 @@ namespace RealismOverhaul.Communications
         public float BaseCost { get; private set; }
         public float CostPerWatt { get; private set; }
 
-        public TechLevel(float efficiency, float gain, float minDataRate, float maxPower, float massPerWatt, float baseMass, float basePower, float frequency, float baseCost, float costPerWatt)
+        public TechLevel(float efficiency, float gain, float minDataRate, float maxDataRate, float maxPower, float massPerWatt, float baseMass, float basePower, float frequency, float baseCost, float costPerWatt)
         {
             Efficiency = efficiency;
             Gain = gain;
             MinDataRate = minDataRate;
+            MaxDataRate = maxDataRate;
             MaxPower = maxPower;
             MassPerWatt = massPerWatt;
             BaseMass = baseMass;
@@ -40,15 +43,15 @@ namespace RealismOverhaul.Communications
 
         private static IList<TechLevel> _techLevels = new List<TechLevel>()
         {
-            new TechLevel(1/18f, -13f, 4, 0.1f, 1.6f, 1, 20.5f, 8415f, 2, 5),
-            new TechLevel(1/13f, -10f, 4, 1, 1.34f, 0.26f, 20.5f, 8415f, 4, 4),
-            new TechLevel(1/10f, -4.5f, 1, 5, 1.16f, 6.9f, 20.5f, 8415f, 30, 3.5f),
-            new TechLevel(3/23f, -2.5f, 8, 5, 1, 20.2f, 20.5f, 8415f, 50, 3),
-            new TechLevel(1/6f, 0f, 8, 10, 0.86f, 17.2f, 20.5f, 8415f, 80, 2.5f),
-            new TechLevel(1/4.5f, 3.2f, 16, 20, 0.75f, 21, 21, 8415f, 120, 2),
-            new TechLevel(1/4f, 6f, 16, 20, 11.6f/18, 30.7f, 21.4f, 8415f, 175, 1.7f),
-            new TechLevel(20/53.7f, 7f, 16, 40, 10.8f/20, 21.3f, 18.3f, 8415f, 75, 0.5f),
-            new TechLevel(41.6f/94.6f, 7f, 16, 100, 5.9f/41.6f, 7.5f, 11.7f, 32050f, 50, 0.4f)
+            new TechLevel(1/18f, -13f, 4, 4, 0.1f, 1.6f, 1, 20.5f, 8415f, 2, 5),
+            new TechLevel(1/13f, -10f, 4, 4, 1, 1.34f, 0.26f, 20.5f, 8415f, 4, 4),
+            new TechLevel(1/10f, -4.5f, 1, 64, 5, 1.16f, 6.9f, 20.5f, 8415f, 30, 3.5f),
+            new TechLevel(3/23f, -2.5f, 8, 64, 5, 1, 20.2f, 20.5f, 8415f, 50, 3),
+            new TechLevel(1/6f, 0f, 8, 4096, 10, 0.86f, 17.2f, 20.5f, 8415f, 80, 2.5f),
+            new TechLevel(1/4.5f, 3.2f, 16, 16384, 20, 0.75f, 21, 21, 8415f, 120, 2),
+            new TechLevel(1/4f, 6f, 16, 131072, 20, 11.6f/18, 30.7f, 21.4f, 8415f, 175, 1.7f),
+            new TechLevel(20/53.7f, 7f, 16, 262144, 40, 10.8f/20, 21.3f, 18.3f, 8415f, 75, 0.5f),
+            new TechLevel(41.6f/94.6f, 7f, 16, 134217728, 100, 5.9f/41.6f, 7.5f, 11.7f, 32050f, 50, 0.4f)
         };
     }
 }
