ab0642d703d6b6483c865b8d405d5aa642b19479
John Vanderbeck <jwvanderbeck@gmail.com>
2021-07-11 12:56:25 -0400

    Update makeMeta

--- makeMeta.py
+++ makeMeta.py
@@ -9,19 +9,19 @@ class DefaultHelpParser(argparse.ArgumentParser):
 HELP_DESC = "Creates neccesary metadata files"
 parser = DefaultHelpParser(description=HELP_DESC)
 parser.add_argument('tag', metavar='tag', type=str, nargs=1,
-                   help='tag of release (e.g. 0.4.6.0')
+                   help='tag of release (e.g. v0.4.6.0')
 
 args = parser.parse_args()
 
 if not args.tag or len(args.tag) < 1:
-    print "ERROR: git tag must be specified and must be in the format major.minor.patch.build-configuration.e.g. 0.4.6.0"
+    print("ERROR: git tag must be specified and must be in the format major.minor.patch.build-configuration.e.g. 0.4.6.0")
     sys.exit(2)
 
 version = args.tag[0].split("v")[1].split(".")
 major = int(version[0])
 minor = int(version[1])
 patch = int(version[2])
-build = 0
+build = int(version[2])
 if len(version) == 4:
 	build = int(version[3])
 # create AVC .version file
@@ -45,8 +45,8 @@ avc = {
 	"KSP_VERSION" :
 	{
 		"MAJOR" : 1,
-		"MINOR" : 2,
-		"PATCH" : 2
+		"MINOR" : 10,
+		"PATCH" : 1
 	}
 }
 with open("RO.version", "w") as f:
