c6ae400020f45e714385ee958e5ca403b58ce22a
NathanKell <nathankell@gmail.com>
2021-07-15 21:56:54 -0700

    Prep for v14.1.3

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.12.8.1")]
-[assembly: AssemblyFileVersion("14.1.1.0")]
+[assembly: AssemblyFileVersion("14.1.3.0")]
