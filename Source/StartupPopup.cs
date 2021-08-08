13d18843c17d7154ca48c879af20cd37623d6cef
Standecco <43384369+Standecco@users.noreply.github.com>
2021-08-08 19:38:18 +0200

    Improve rotation popup (#2501)
    * Add option to disable rotation popup, slightly more impartial message
    
    * Place file into PluginData

--- Source/StartupPopup.cs
+++ Source/StartupPopup.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using UnityEngine;
 
 namespace RealismOverhaul
@@ -6,10 +8,16 @@ namespace RealismOverhaul
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     class StartupPopup : MonoBehaviour
     {
+        private const string PreferenceFileName = "RORotationPopupLock";
+        private static string PreferenceFilePath => Assembly.GetExecutingAssembly().Location + "PluginData/" + PreferenceFileName;
+        
         public void Start()
         {
+            if (File.Exists(PreferenceFilePath)) return;
+
             bool hasPR = false;
             bool hasMRCS = false;
+
             foreach (var a in AssemblyLoader.loadedAssemblies)
             {
                 if (a.name == "PersistentRotation")
@@ -23,18 +31,38 @@ namespace RealismOverhaul
             }
 
             
-
-             if (hasPR || hasMRCS)
+            if (hasPR || hasMRCS)
             {
                 String message = "Realism Overhaul now contains its own, light, implementation of continuing vessel rotation during timewarp for when Principia is not installed. We've detected you have:\n\n";
                 if (hasPR)
                     message += "* Persistent Rotation\n";
                 if (hasMRCS)
                     message += "* MandatoryRCS\n";
-                message += "\ninstalled. Please quit, remove " + ((hasPR && hasMRCS) ? "them" : "it") + ", and relaunch KSP.\n\nBut don't worry, RO's own implementation will be disabled until you do.";
+                message += "\ninstalled. It is suggested to quit, remove " + ((hasPR && hasMRCS) ? "them" : "it") + ", and relaunch KSP.\n\nBut don't worry, RO's own implementation will be disabled until you do.";
+                
 
-                PopupDialog.SpawnPopupDialog(new Vector2(0, 0), new Vector2(0, 0), "ROStartupDialog", "Realism Overhaul", message, "OK", true, HighLogic.UISkin);
+                PopupDialog.SpawnPopupDialog(
+                    new Vector2(0, 0),
+                    new Vector2(0, 0),
+                    new MultiOptionDialog(
+                        "ROStartupDialog",
+                        message,
+                        "Realism Overhaul",
+                        HighLogic.UISkin,
+                        new DialogGUIVerticalLayout(
+                            new DialogGUIButton("Don't show again", RememberPreference, true),
+                            new DialogGUIButton("Ok", () => {}, true )
+                            )
+                        ),
+                true,
+                    HighLogic.UISkin);
             }
+        }
+
+        private static void RememberPreference()
+        {
+            // create empty file
+            File.Create(PreferenceFilePath).Close();
         }
     }
 }
