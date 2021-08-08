8d81d3fa10c5136b5c7c10fa54982d7eb67c754c
NathanKell <nathankell@gmail.com>
2021-08-07 22:48:08 -0700

    Add spacing to popup

--- Source/StartupPopup.cs
+++ Source/StartupPopup.cs
@@ -26,12 +26,12 @@ namespace RealismOverhaul
 
              if (hasPR || hasMRCS)
             {
-                String message = "Realism Overhaul now contains its own, light, implementation of continuing vessel rotation during timewarp for when Principia is not installed. We've detected you have:\n";
+                String message = "Realism Overhaul now contains its own, light, implementation of continuing vessel rotation during timewarp for when Principia is not installed. We've detected you have:\n\n";
                 if (hasPR)
                     message += "* Persistent Rotation\n";
                 if (hasMRCS)
                     message += "* MandatoryRCS\n";
-                message += "installed. Please quit, remove " + ((hasPR && hasMRCS) ? "them" : "it") + ", and relaunch KSP.\n\nBut don't worry, RO's own implementation will be disabled until you do.";
+                message += "\ninstalled. Please quit, remove " + ((hasPR && hasMRCS) ? "them" : "it") + ", and relaunch KSP.\n\nBut don't worry, RO's own implementation will be disabled until you do.";
 
                 PopupDialog.SpawnPopupDialog(new Vector2(0, 0), new Vector2(0, 0), "ROStartupDialog", "Realism Overhaul", message, "OK", true, HighLogic.UISkin);
             }
