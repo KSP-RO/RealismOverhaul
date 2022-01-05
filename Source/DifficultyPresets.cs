6bbc9ae6f2f6c0125a78c252f9fc8a64a05282ef
stonesmileGit <luddeboda99@gmail.com>
2022-01-05 21:18:40 +0100

    Remove DifficultyPresetChanger

--- Source/DifficultyPresets.cs
+++ Source/DifficultyPresets.cs
@@ -4,34 +4,6 @@ using UnityEngine;
 
 namespace RealismOverhaul
 {
-    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
-    public class DifficultyPresetChanger : MonoBehaviour
-    {
-        public void Awake()
-        {
-            ConfigNode paramsNode = null;
-            foreach (ConfigNode n in GameDatabase.Instance.GetConfigNodes("GAMEPARAMETERS"))
-                paramsNode = n;
-
-            if (paramsNode == null)
-            {
-                Debug.LogError("[RealismOverhaul] Could not find GAMEPARAMETERS node.");
-                return;
-            }
-
-            GameParameters.SetDifficultyPresets();
-
-            foreach (KeyValuePair<GameParameters.Preset, GameParameters> kvp in GameParameters.DifficultyPresets)
-            {
-                ConfigNode n = paramsNode.GetNode(kvp.Key.ToString());
-                if (n != null)
-                    kvp.Value.Load(n);
-            }
-
-            Debug.Log("[RealismOverhaul] Reset difficulty presets.");
-        }
-    }
-
     public class RealismOverhaulSettings : GameParameters.CustomParameterNode
     {
         public override string Title { get { return "General Settings"; } }
@@ -39,7 +11,7 @@ namespace RealismOverhaul
         public override string Section { get { return "RealismOverhaul"; } }
         public override string DisplaySection { get { return "RealismOverhaul"; } }
         public override int SectionOrder { get { return 1; } }
-        public override bool HasPresets { get { return true; } }
+        public override bool HasPresets { get { return false; } }
 
         [GameParameters.CustomParameterUI("Speculative Level", toolTip = "What parts are available.\nReal = Only real hardware is available.\nProposal = Real proposals that were never built are available.\nScifi = Scifi parts are available.")]
         public RealismOverhaulspeculative speculativeLevel = RealismOverhaulspeculative.real;
