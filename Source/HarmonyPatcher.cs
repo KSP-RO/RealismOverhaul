8daf20e37d326cb9c78b9d1bdc1489a8577b61c9
siimav <siim.aaver@gmail.com>
2023-01-20 14:36:25 +0200

    Add ejection seat partmodule

---
+++ Source/HarmonyPatcher.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace RealismOverhaul
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class HarmonyPatcher : MonoBehaviour
+    {
+        internal void Start()
+        {
+            var harmony = new HarmonyLib.Harmony("RO.HarmonyPatcher");
+            harmony.PatchAll();
+        }
+    }
+}
