ed37bc535dd285504ce6ffde6ff24dae42f8f5b1
NathanKell <nathankell@gmail.com>
2023-10-22 14:56:10 -0700

    Patch resource rates and RCS/Engine propellant rates to use ResourceUnitInfo data

---
+++ Source/Harmony/ModuleRCS.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using KSP.Localization;
+
+namespace RealismOverhaul.Harmony
+{
+    [HarmonyPatch(typeof(ModuleRCS))]
+    internal class PatchModuleRCS
+    {
+        private static string GetInfo(ModuleRCS mod)
+        {
+            string output = Localizer.Format("#autoLOC_217936", mod.thrusterPower.ToString("0.0###"));
+            output += Localizer.Format("#autoLOC_217937", mod.atmosphereCurve.Evaluate(1f).ToString("0.###"), mod.atmosphereCurve.Evaluate(0f).ToString("0.###"));
+            output += ModuleRCS.cacheAutoLOC_217939;
+            double massFlow = 0d;
+            foreach (var propellant in mod.propellants)
+            {
+                double flow = mod.getMaxFuelFlow(propellant, mod.atmosphereCurve.Evaluate(0f));
+                massFlow += flow * propellant.resourceDef.density;
+                output += ResourceUnits.PrintRate(flow, propellant.id, false, null, propellant, true);
+            }
+            if (massFlow > 0d)
+                output += Localizer.Format("#autoLOC_900654") + " " + Localizer.Format("#autoLOC_6001048", ResourceUnits.PrintMassRate(massFlow)) + "\n";
+
+            if (!mod.moduleIsEnabled)
+            {
+                output += ModuleRCS.cacheAutoLOC_217950;
+            }
+            return output;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch("GetInfo")]
+        internal static bool Prefix_GetInfo(ModuleRCS __instance, ref string __result)
+        {
+            __result = GetInfo(__instance);
+            return false;
+        }
+    }
+}
---
+++ Source/Harmony/ModuleResource.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using System.Reflection;
+
+namespace RealismOverhaul.Harmony
+{
+    [HarmonyPatch(typeof(ModuleResource))]
+    internal class PatchModuleResource
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("PrintRate", typeof(bool), typeof(double))]
+        internal static bool Prefix_PrintRate(ModuleResource __instance, bool showFlowMode, double mult, ref string __result)
+        {
+            __result = ResourceUnits.PrintRate(__instance.rate * mult, __instance.id, showFlowMode, __instance);
+            return false;
+        }
+    }
+}
