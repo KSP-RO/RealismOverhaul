1807df91894f9f665ab78111eb6987d2b1d0d4ec
siimav <1120038+siimav@users.noreply.github.com>
2024-06-20 00:41:10 +0300

    Consider all KSP 1.12 revisions the same in compatibility checks (#2972)

--- Source/Harmony/KSPUtil.cs
+++ Source/Harmony/KSPUtil.cs
@@ -13,5 +13,17 @@ namespace RealismOverhaul.Harmony
             __result = ResourceUnits.PrintSI(amount, unitName, sigFigs, longPrefix);
             return false;
         }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("CheckVersion", typeof(int), typeof(int), typeof(int), typeof(int), typeof(int), typeof(int))]
+        internal static void CheckVersion(int version_major, int version_minor, int version_revision, int lastMajor, int lastMinor, int lastRev, ref VersionCompareResult __result)
+        {
+            // it's silly to differentiate between KSP 1.12 revisions
+            if (__result == VersionCompareResult.INCOMPATIBLE_TOO_LATE &&
+                version_major == 1 && version_minor == 12)
+            {
+                __result = VersionCompareResult.COMPATIBLE;
+            }
+        }
     }
 }
