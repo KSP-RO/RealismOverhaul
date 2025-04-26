88f25f0a3eb42fb80321d7d5c9084c6847de42bf
siimav <siim.aaver@gmail.com>
2025-04-27 02:54:43 +0300

    Nerf heatshields when their ablator runs out

---
+++ Source/HSClobbererSettings.cs
@@ -0,0 +1,29 @@
+﻿using ROUtils.DataTypes;
+
+namespace RealismOverhaul
+{
+    public class HSClobbererSettings : ConfigNodePersistenceBase, IConfigNode
+    {
+        [Persistent]
+        public float skinIntCondIncreaseMult = 200f;
+        [Persistent]
+        public float skinSkinCondIncreaseMult = 250f;
+        [Persistent]
+        public double ablatorThreshold = 0.01;
+        [Persistent]
+        public float effectTime = 5f;
+        [Persistent]
+        public PersistentHashSetValueType<string> excludedParts = new PersistentHashSetValueType<string>();
+
+        public static HSClobbererSettings LoadHSClobbererSettings()
+        {
+            var settings = new HSClobbererSettings();
+            foreach (ConfigNode n in GameDatabase.Instance.GetConfigNodes("HS_CLOBBERER_SETTINGS"))
+            {
+                settings.Load(n);
+            }
+
+            return settings;
+        }
+    }
+}
